eafe1e2f6a2e44344fb7e795e77dca490c5118b4
tomit4 <mosssap@gmail.com>
2026-03-22 10:35:57 -0700

    :construction: Setup for sqlite instead

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-import mysql.connector
+import sqlite3
 
-conn = mysql.connector.connect(host="localhost", user="root", password="rootpassword")
+con = sqlite3.connect("tutorial.db")
+cur = con.cursor()
 
-cursor = conn.cursor()
+cur.execute("""
+            CREATE TABLE IF NOT EXISTS movies(
+                movie_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                year TEXT NOT NULL,
+                score REAL)
+            """)
 
-cursor.execute("CREATE DATABASE employee_db")
-conn.close()
+movies = [
+    ("Monty Python and the Holy Grail", 1975, 8.2),
+    ("And Now for Something Completely Different", 1971, 7.5),
+]
+
+cur.executemany("INSERT INTO movies(title, year, score) VALUES (?, ?, ?)", movies)
+
+con.commit()
+
+res = cur.execute("SELECT * FROM movies")
+print("res.fetchall() :=> ", res.fetchall())
